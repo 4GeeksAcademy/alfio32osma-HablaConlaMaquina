4a28d82260a6e3d4fe23fc08c5786907f6e45c1a
alfio32osma <alfio32osma@gmail.com>
2026-05-16 17:50:54 +0000

    añadir context & readme

---
+++ my-app/test-groq.js
@@ -0,0 +1,29 @@
+export const requestWithAuthentication = async () => {
+  // Creo la url ficticia de un recurso protegido
+  let url = `https://webHost.com/api/protected`  
+
+  // Leo el valor de la API_KEY que guardé en el archivo .env
+  let apiKey = import.meta.env.API_SECRET_KEY
+  
+  // Creo un objeto denominado 'options' en el cual defino la Autenticación, agregando la API_Key
+  let options = {
+    headers: {
+      Authorization: `Bearer ${apiKey}`
+    }
+  }
+  
+  // Ejecuto el fetch() con la url y opciones definidas previamente
+  const response = await fetch(url, options);  
+  
+  // Valido el resultado de la petición
+  if (!response.ok) {
+    console.log('Error', response.status, response.statusText)
+    return false
+  }
+  
+  // Extraigo los datos JSON de la petición
+  const data = await response.json()
+  
+  // Retorno los datos obtenidos
+  return data
+}
